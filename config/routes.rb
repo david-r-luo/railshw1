9eb1e3c0f321c6bc3aa62d29f3b99eed5e8268fd
David Luo <davidluo@berkeley.edu>
2018-10-07 23:54:29 -0700

    Submitting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "main#index"
+  get 'cities/view', to: 'cities#view'
+  get 'cities/create', to: 'cities#create'
+  get 'cities/update', to: 'cities#update'
 end
